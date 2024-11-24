9decda694300c0aa186e1a694ca0bdde4e06801d
Bruno Marques <marques.bs@outlook.com.br>
2024-11-24 18:23:20 -0300

    Problema solucionado

--- estruturaSequencial/Ex04/Program.cs
+++ estruturaSequencial/Ex04/Program.cs
@@ -1,9 +1,19 @@
-﻿namespace Ex04;
+﻿using System;
+using System.Globalization;
+
+namespace Ex04;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int funcionario = int.Parse(Console.ReadLine());
+        int horasTrabalhadas = int.Parse(Console.ReadLine());
+        double valorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double salario = horasTrabalhadas * valorHora;
+
+        System.Console.WriteLine("NUMBER = " + funcionario);
+        System.Console.WriteLine("SALARY = U$ " + salario.ToString("F2"), CultureInfo.InvariantCulture);
     }
 }
