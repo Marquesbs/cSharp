28a248aa280bcdabece54e4d648f8fdabe18a437
Bruno Marques <marques.bs@outlook.com.br>
2024-11-24 18:09:49 -0300

    resolução do problema.

--- estruturaSequencial/Ex03/Program.cs
+++ estruturaSequencial/Ex03/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace Ex03;
+﻿using System;
+using System.Globalization;
+
+namespace Ex03;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int num1 = int.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+        int num2 = int.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+        int num3 = int.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+        int num4 = int.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+        int diferenca = ((num1 * num2) - (num3 * num4));
+
+        System.Console.WriteLine("DIFERENCA = " + diferenca.ToString(CultureInfo.InvariantCulture));
     }
 }
