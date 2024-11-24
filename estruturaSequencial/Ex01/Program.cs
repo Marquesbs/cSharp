bc23ea1c22251b8544a4a81cc35d47ec146688e1
Bruno Marques <marques.bs@outlook.com.br>
2024-11-24 16:54:46 -0300

    Exercício 01: Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma mensagem explicativa, conforme exemplos.

---
+++ estruturaSequencial/Ex01/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Ex01;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num1 = int.Parse(Console.ReadLine());
+        int num2 = int.Parse(Console.ReadLine());
+
+        int soma = num1 + num2;
+
+        System.Console.WriteLine("SOMA = " + soma);
+    }
+}
