9253faf44af0404cb69f7ea2f24e05df23cfdc58
Bruno Marques <marques.bs@outlook.com.br>
2024-11-25 10:47:45 -0300

    Exercício 05: Fazer um programa para ler o código de uma peça 1, o número de peças 1, o valor unitário de cada peça 1, o código de uma peça 2, o número de peças 2 e o valor unitário de cada peça 2. Calcule e mostre o valor a ser pago.

--- estruturaSequencial/Ex05/Program.cs
+++ estruturaSequencial/Ex05/Program.cs
@@ -1,9 +1,23 @@
-﻿namespace Ex05;
+﻿using System.Globalization;
+
+namespace Ex05;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string[] vetor1 = Console.ReadLine().Split(' ');
+        int codigoPeca1 = int.Parse(vetor1[0]);
+        int quantidadePeca1 = int.Parse(vetor1[1]);
+        double valorPeca1 = double.Parse(vetor1[2], CultureInfo.InvariantCulture);
+
+        string[] vetor2 = Console.ReadLine().Split(' ');
+        int codigoPeca2 = int.Parse(vetor2[0]);
+        int quantidadePeca2 = int.Parse(vetor2[1]);
+        double valorPeca2 = double.Parse(vetor2[2], CultureInfo.InvariantCulture);
+
+        double valorTotal = (quantidadePeca1 * valorPeca1) + (quantidadePeca2 * valorPeca2);
+
+        System.Console.WriteLine("VALOR A PAGAR: R$ " + valorTotal.ToString("F2"), CultureInfo.InvariantCulture);
     }
 }
