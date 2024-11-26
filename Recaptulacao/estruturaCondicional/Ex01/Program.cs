fd16de25ec1a4bcdb1b7a6d4bb20bfe69210b0fe
Bruno Marques <marques.bs@outlook.com.br>
2024-11-26 20:55:26 -0300

    Exercício 01: Fazer um programa para ler um número inteiro, e depois dizer se este número é negativo ou não.

---
+++ Recaptulacao/estruturaCondicional/Ex01/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Ex01;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numero = int.Parse(Console.ReadLine());
+
+        if (numero >= 0)
+        {
+            System.Console.WriteLine("NAO NEGATIVO");
+        }
+        else
+        {
+            System.Console.WriteLine("NEGATIVO");
+        }
+    }
+}
