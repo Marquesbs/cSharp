f12e90ed87d27a2ed8170e309ce14d9706d5bd9e
Bruno Marques <marques.bs@outlook.com.br>
2024-11-26 20:58:43 -0300

    Exercício 02: Fazer um programa para ler um número inteiro e dizer se este número é par ou ímpar.

---
+++ Recaptulacao/estruturaCondicional/Ex02/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ex02;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numero = int.Parse(Console.ReadLine());
+        int calcular = numero % 2;
+
+        if (calcular == 0)
+        {
+            System.Console.WriteLine("PAR");
+        }
+        else
+        {
+            System.Console.WriteLine("IMPAR");
+        }
+        
+    }
+}
