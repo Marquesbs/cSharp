26443e505200d418ed836cfb6dc1b66c634d415d
Bruno Marques <marques.bs@outlook.com.br>
2024-12-02 21:39:18 -0300

    Strings
    Novo programa de reforço de strings

---
+++ Recaptulacao/Strings/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Strings;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var names = new[] { "Bruno", "Felipe", "Emillia" };
+
+        foreach (var name in names)
+        {
+            Console.WriteLine($"Hello {name.ToUpper()}!");
+        }
+    }
+}
