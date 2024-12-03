cfb6e7e38ae8a9f2c6faad44b903b8e8baf39fb6
Bruno Marques <marques.bs@outlook.com.br>
2024-12-03 20:37:24 -0300

    Numbers in C#

--- Recaptulacao/Numeros/Program.cs
+++ Recaptulacao/Numeros/Program.cs
@@ -9,7 +9,18 @@ class Program
         //funcao checked verifica o erro e retorna o valor.
         //long c = checked(a + b);
         long d = (long)a + (long)b;
-        System.Console.WriteLine(d);
+        System.Console.WriteLine("soma integer = " + d);
         //System.Console.WriteLine(c);
+
+        float flutuante1 = 42.1F;
+        double  flutuante2 = 4.33; //natural type
+        double result = flutuante1 + flutuante2;
+        System.Console.WriteLine("float + double = " + result);
+
+
+        decimal numero1 = 42.1M; //explicit type
+        decimal numero2 = 4.33M;
+        decimal resultDecimal = numero1 + numero2;
+        System.Console.WriteLine("Resultado soma decimal = " + resultDecimal);
     }
 }
--- Recaptulacao/Numeros/obj/Debug/net9.0/Numeros.AssemblyInfo.cs
+++ Recaptulacao/Numeros/obj/Debug/net9.0/Numeros.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Numeros")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ca2e3a04149b44cb5d980fd5035e0ebb1d4782f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e4534a4b935385a0f1c01e32986c04781e66ace")]
 [assembly: System.Reflection.AssemblyProductAttribute("Numeros")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Numeros")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
