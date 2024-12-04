81eb987fac060740dabc356dbc6ff884c1da51c9
Bruno Marques <marques.bs@outlook.com.br>
2024-12-03 21:09:08 -0300

    Resolução

--- Recaptulacao/estruturaCondicional/Ex04/Program.cs
+++ Recaptulacao/estruturaCondicional/Ex04/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string[] vetor = Console.ReadLine().Split(' ');
+        int horaInicial = int.Parse(vetor[0]);
+        int horaFinal = int.Parse(vetor[1]);
+        int calcularHorasDeJogo = 0;
+
+        if (horaInicial > horaFinal)
+        {
+            calcularHorasDeJogo = (24 - horaInicial) + horaFinal;
+            System.Console.WriteLine($"O JOGO DUROU {calcularHorasDeJogo} HORA(S)");
+        }
+        else if (horaInicial < horaFinal)
+        {
+            calcularHorasDeJogo = horaFinal - horaInicial;
+            System.Console.WriteLine($"O JOGO DUROU {calcularHorasDeJogo} HORA(S)");
+        }
+        else
+        {
+            calcularHorasDeJogo = 24;
+            System.Console.WriteLine($"O JOGO DUROU {calcularHorasDeJogo} HORA(S)");
+        }
+        
     }
 }
