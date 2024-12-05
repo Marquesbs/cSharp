db0f1d4f9f8c2bcf98f074e1c262c5dcf4657e33
Bruno Marques <marques.bs@outlook.com.br>
2024-12-05 11:07:25 -0300

    Exercícioo 07: Leia 2 valores com uma casa decimal (x e y), que devem representar as coordenadas de um ponto em um plano. A seguir, determine qual o quadrante ao qual pertence o ponto, ou se está sobre um dos eixos cartesianos ou na origem (x = y = 0). Se o ponto estiver na origem, escreva a mensagem “Origem”. Se o ponto estiver sobre um dos eixos escreva “Eixo X” ou “Eixo Y”, conforme for a situação.

--- Recaptulacao/estruturaCondicional/Ex07/Program.cs
+++ Recaptulacao/estruturaCondicional/Ex07/Program.cs
@@ -1,9 +1,43 @@
-﻿namespace Ex07;
+﻿using System;
+using System.Globalization;
+
+namespace Ex07;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string[] vetor = Console.ReadLine().Split(' ');
+        float valorX = float.Parse(vetor[0], CultureInfo.InvariantCulture);
+        float valorY = float.Parse(vetor[1], CultureInfo.InvariantCulture);
+
+        if(valorX == 0.0 && valorY == 0.0)
+        {
+            System.Console.WriteLine("Origem");
+        }
+        
+        else if (valorX > 0.0)
+        {
+            if (valorY > 0.0)
+            {
+                System.Console.WriteLine("Q1");
+            }
+            else
+            {
+                System.Console.WriteLine("Q4");
+            }
+        }
+        
+        else if (valorX < 0.0)
+        {
+            if (valorY > 0.0)
+            {
+                System.Console.WriteLine("Q2");
+            }
+            else
+            {
+                System.Console.WriteLine("Q3");
+            }
+        }
     }
 }
