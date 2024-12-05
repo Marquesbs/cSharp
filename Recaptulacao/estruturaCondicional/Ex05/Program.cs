2cd01c3247fab870b3996453c1e9a0ac626c8239
Bruno Marques <marques.bs@outlook.com.br>
2024-12-05 10:39:28 -0300

    Exercício 05: Com base na tabela abaixo, escreva um programa que leia o código de um item e a quantidade deste item. A seguir, calcule e mostre o valor da conta a pagar.

---
+++ Recaptulacao/estruturaCondicional/Ex05/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Ex05;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] vetor = Console.ReadLine().Split(' ');
+        int codigoItem = int.Parse(vetor[0]);
+        int quantidade = int.Parse(vetor[1]);
+        double valorTotal = 0.0;
+
+        if (codigoItem == 1)
+        {
+            valorTotal = 4.00 * quantidade; 
+        }
+        else if (codigoItem == 2)
+        {
+            valorTotal = 4.50 * quantidade;
+        }
+        else if(codigoItem == 3)
+        {
+            valorTotal = 5.00 * quantidade;
+        }
+        else if(codigoItem == 4)
+        {
+            valorTotal = 2.00 * quantidade;
+        }
+        else if(codigoItem == 5)
+        {
+            valorTotal = 1.50 * quantidade;
+        }
+        System.Console.WriteLine($"Total: R$ {valorTotal:F2}");
+    }
+}
