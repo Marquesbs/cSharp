64b60cf60675b883a0489b7ca6d19078fc6376ce
Bruno Marques <marques.bs@outlook.com.br>
2024-12-07 10:41:58 -0300

    Exercício 03 - Resolvido
    Leia 1 valor inteiro N, que representa o número de casos de teste que vem a seguir. Cada caso de teste consiste
    de 3 valores reais, cada um deles com uma casa decimal. Apresente a média ponderada para cada um destes
    conjuntos de 3 valores, sendo que o primeiro valor tem peso 2, o segundo valor tem peso 3 e o terceiro valor tem
    peso 5.

---
+++ Recaptulacao/estruturaFor/Ex03/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lê o número de casos de teste
+            int casosDeTeste = int.Parse(Console.ReadLine());
+            
+            // Cria um vetor para armazenar as médias
+            double[] mediasFinais = new double[casosDeTeste];
+            
+            // Loop para processar cada caso de teste
+            for (int i = 0; i < casosDeTeste; i++)
+            {
+                // Lê a entrada de valores separados por espaço e armazena em um vetor de strings
+                string[] valores = Console.ReadLine().Split(' ');
+                
+                // Converte os valores do vetor para double
+                double valor1 = double.Parse(valores[0], CultureInfo.InvariantCulture);
+                double valor2 = double.Parse(valores[1], CultureInfo.InvariantCulture);
+                double valor3 = double.Parse(valores[2], CultureInfo.InvariantCulture);
+                
+                // Calcula a média ponderada
+                double media = ((valor1 * 2) + (valor2 * 3) + (valor3 * 5)) / 10;
+
+                // Armazena a média calculada no vetor de médias finais
+                mediasFinais[i] = media;
+            }
+            
+            // Exibe as médias calculadas usando o laço `for`
+            for (int i = 0; i < casosDeTeste; i++)
+            {
+                Console.WriteLine(mediasFinais[i].ToString("F1", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
