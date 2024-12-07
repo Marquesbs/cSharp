070198b63acb4aa41d22d222de7de7e530d74f1e
Bruno Marques <marques.bs@outlook.com.br>
2024-12-07 09:54:29 -0300

    Exercício 02: Leia um valor inteiro N. Este valor será a quantidade de valores inteiros X que serão lidos em seguida. Mostre quantos destes valores X estão dentro do intervalo [10,20] e quantos estão fora do intervalo, mostrando essas informações conforme exemplo (use a palavra "in" para dentro do intervalo, e "out" para fora do intervalo).

---
+++ Recaptulacao/estruturaFor/Ex02/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Ex02;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int quantidadeDeValores = int.Parse(Console.ReadLine());
+        int auxiliar = 0;
+        int dentro = 0;
+        int fora = 0;
+
+        for (int i = 0;i < quantidadeDeValores; i++)
+        {
+            auxiliar = int.Parse(Console.ReadLine());
+            if (auxiliar >= 10 && auxiliar <= 20)
+            {
+                dentro++;
+            }
+            else
+            {
+                fora++;
+            }
+        }
+        System.Console.WriteLine($"{dentro} in\n{fora} out");
+    }
+}
