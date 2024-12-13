4fa89202a8b8b989270f00d2fb6bf5fed134b84c
Bruno Marques <marques.bs@outlook.com.br>
2024-12-13 15:08:19 -0300

    Exercício 06: Ler um número inteiro N e calcular todos os seus divisores.

---
+++ Recaptulacao/estruturaFor/Ex06/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Ex06;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numero = int.Parse(Console.ReadLine());
+
+        for (int divisor = 1; divisor <= numero; divisor++)
+        {
+            if (numero % divisor == 0)
+            {
+                System.Console.WriteLine(divisor);
+            }
+        }
+    }
+}
