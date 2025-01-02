a4e5d91301d4a9382a4c9609f65ba87ed5c565eb
Bruno Marques <marques.bs@outlook.com.br>
2025-01-02 11:50:37 -0300

    Alteração de programa

--- Recaptulacao/PrimeiroPrograma/Program.cs
+++ Recaptulacao/PrimeiroPrograma/Program.cs
@@ -1,9 +1,41 @@
-﻿namespace primeiroPrograma;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace primeiroPrograma;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        /* 
+        Console.WriteLine("Olá mundo!"); //imprime um valor na tela
+
+        Console.ForegroundColor = ConsoleColor.Blue; //Altera a cor da fonte
+        Console.WriteLine("Meu primeiro projeto.");
+
+        Console.ReadKey(); //Aguarda o pressionamento de alguma tecla para continuar
+        */
+
+        int NumeroDigitado;
+
+        Console.Write("Insira um número: "); //Exibe a mensagem
+
+        NumeroDigitado = Convert.ToInt32(Console.ReadLine()); //Lê e converte para int o número informado pelo usuário
+
+        if (NumeroDigitado % 2 == 0)
+        {
+            //Número Par
+            Console.WriteLine("Par");
+        }
+        else
+        {
+            //Número Impar
+            Console.WriteLine("Impar");
+        }
+
+        Console.ReadKey();
     }
 }
+
